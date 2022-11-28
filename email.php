04660f8d3460c8134949388dc92868dc22578aed
Pedro Rebelo <108408325+pfgrebelo@users.noreply.github.com>
2022-11-28 23:40:01 +0000

    Update email.php

--- email.php
+++ email.php
@@ -32,12 +32,12 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.sapo.pt';                     //Set the SMTP server to send through
+    $mail->Host       = SMTP_HOST;                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'pedrocesae@sapo.pt';                     //SMTP username
-    $mail->Password   = 'P4ssw0rd+';                               //SMTP password
+    $mail->Username   = SMTP_USERNAME;                     //SMTP username
+    $mail->Password   = SMTP_PASSWORD;                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Port       = PORT;                                   //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     $mail->CharSet = 'UTF-8';
 
     //Recipients
